4aacd114fc4c4b7625b07c51024947bb86935916
alvarohu <alvarojhu408@gmail.com>
2025-06-22 14:39:35 -0500

    adding odds

--- writing_generator.py
+++ writing_generator.py
@@ -6,6 +6,17 @@ import datetime as dt
 def main():
     st.title('Category Generator')
 
+    odds_500 = 0.1
+    odds_1000 = 0.3
+    odds_1500 = 0.3
+    odds_2000 = 0.2
+    odds_2500 = 0.05
+    odds_3000 = 0.05
+
+    st.info(f'Odds of story length are {odds_500*100}% for 500 words, {odds_1000*100}% each for 1000 and '
+            f'1500, {odds_2000*100}% for 2000 words, and {odds_2500*100}% each for 2500 and 3000 words. '
+            'There are 5 Days assigned per 100 words of story selected. ')
+
     randomize = st.button('Randomize')
     member_list = ['Alvaro','Elijah','Favier','Raffy']
 
@@ -24,11 +35,23 @@ def main():
         action = member_list[2]
         setting = member_list[3]
 
-        word_count = np.random.randint(1,6)*500
-        weeks_to_add = dt.timedelta(weeks=word_count/500)
-        days_to_add = dt.timedelta(days=1)
+        rand = np.random.random()
+        if rand < odds_500:
+            word_count = 500
+        elif rand >= odds_500 and rand < odds_500 + odds_1000:
+            word_count = 1000
+        elif rand >= odds_500 + odds_1000 and rand < odds_500 + odds_1000 + odds_1500:
+            word_count = 1500
+        elif rand >= odds_500 + odds_1000 + odds_1500 and rand < odds_500 + odds_1000 + odds_1500 + odds_2000:
+            word_count = 2000
+        elif rand >= odds_500 + odds_1000 + odds_1500 + odds_2000 and rand < odds_500 + odds_1000 + odds_1500 + odds_2000 + odds_2500:
+            word_count = 2500
+        else:
+            word_count = 3000
 
-        due_date = dt.date.today() + weeks_to_add + days_to_add
+        days_to_add = dt.timedelta(days=(word_count/100)+1)
+
+        due_date = dt.date.today() + days_to_add
 
     st.write('Object / Noun: {}'.format(object), font = 'Helvetica 16 bold')
 
